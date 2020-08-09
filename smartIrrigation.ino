847dd4e15c3b04f8f45bdc183e6b009165b6a23c
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-08-09 21:52:13 +0530

    Update smartIrrigation.ino
    Interfacing a Soil moisture sensor FC-28 with Arduino. This sensor measures the volumetric content of water inside the soil and gives us the moisture level as output. The sensor is equipped with both analog and digital output, so it can be used in both analog and digital mode. In this article, we are going to interface the sensor in both modes.

--- smartIrrigation.ino
+++ smartIrrigation.ino
@@ -1,13 +1,20 @@
 int sensor_pin = A0; // Soil Sensor input at Analog PIN A0
-int output_value ;
+int output_value ; 
+//First of all, we have defined two variables;
+//one for the soil moisture sensor pin and the other for storing the output of the sensor.
 void setup() {
   // put your setup code here, to run once:
   pinMode(4,OUTPUT);
+  //In the setup function, the “Serial.begin(9600)” command will help in communication between
+  //the Arduino and serial monitor. Then, we will print the “Reading From the Sensor …” on the serial monitor.
   Serial.begin(9600);
   Serial.println("Reading From the Sensor ...");
   delay(2000);
 }
-
+//In the loop function, we will read from the sensor analog pin and will store the values in the “output_ value”
+//variable. Then, we will map the output values to 0-100, because the moisture is measured in percentage. 
+//When we took the readings from the dry soil, then the sensor value was 550 and in the wet soil, the sensor value 
+//was 10. So, we mapped these values to get the moisture. After that, we printed these values on the serial monitor.
   void loop() {
   // put your main code here, to run repeatedly:
    output_value= analogRead(sensor_pin);
